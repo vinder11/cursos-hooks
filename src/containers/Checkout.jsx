b96ba50997c32bbfca163e6de7a11703a468fe70
Dalvinder <ss_vinder@hotmail.com>
2021-03-10 12:21:27 -0400

    seo

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import AppContext from '../context/AppContext';
 import '../styles/components/Checkout.css';
@@ -17,32 +18,52 @@ const Checkout = () => {
   };
 
   return (
-    <div className='Checkout'>
-      <div className='Checkout-content'>
-        {cart.length > 0 ? <h3>Lista de pedidos: </h3> : <h3>Sin lista de pedidos: </h3>}
+    <>
+      <Helmet>
+        <title>Platzy Conf Merch - Productos</title>
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@TU_USER' />
+        <meta name='twitter:creator' content='@TU_USER' />
+        <meta name='twitter:title' content='Platzi Conf Store' />
+        <meta name='twitter:description' content='Platzi Conf Store' />
+        <meta name='twitter:image' content='https://s3.amazonaws.com/gndx.dev/gndxdev.png' />
+        <meta property='og:title' content='Platzi Conf Store' />
+        <meta property='og:description' content='Platzi Conf Store' />
+        <meta property='og:image' content='https://s3.amazonaws.com/gndx.dev/gndxdev.png' />
+        <meta property='og:url' content='platzistore.xyz' />
+        <meta property='og:site_name' content='Platzi Conf Store' />
+        <meta property='og:locale' content='es_ES' />
+        <meta property='og:type' content='article' />
+        <meta property='fb:app_id' content='ID_APP_FACEBOOK' />
+      </Helmet>
 
-        {cart.map((item) => (
-          <div className='Checkout-item' key={item.id}>
-            <div className='Checkout-element'>
-              <h4>{item.title}</h4>
-              <span>${item.price}</span>
+      <div className='Checkout'>
+        <div className='Checkout-content'>
+          {cart.length > 0 ? <h3>Lista de pedidos: </h3> : <h3>Sin lista de pedidos: </h3>}
+
+          {cart.map((item) => (
+            <div className='Checkout-item' key={item.id}>
+              <div className='Checkout-element'>
+                <h4>{item.title}</h4>
+                <span>${item.price}</span>
+              </div>
+              <button type='button' onClick={() => handleRemove(item)}>
+                <i className='fas fa-trash-alt' title='Eliminar' />
+              </button>
             </div>
-            <button type='button' onClick={() => handleRemove(item)}>
-              <i className='fas fa-trash-alt' title='Eliminar' />
-            </button>
-          </div>
-        ))}
-      </div>
-
-      {cart.length > 0 && (
-        <div className='Checkout-sidebar'>
-          <h3>{`Precio Total: ${handleSumTotal()}`}</h3>
-          <Link to='/checkout/information'>
-            <button type='button'>Continuar pedido</button>
-          </Link>
+          ))}
         </div>
-      )}
-    </div>
+
+        {cart.length > 0 && (
+          <div className='Checkout-sidebar'>
+            <h3>{`Precio Total: ${handleSumTotal()}`}</h3>
+            <Link to='/checkout/information'>
+              <button type='button'>Continuar pedido</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
